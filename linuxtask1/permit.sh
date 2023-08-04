12b737f5b0a92da9755427eaf3012ba76d1b49f3
pirate <132884539+GenericFace@users.noreply.github.com>
2023-08-04 14:09:44 +0530

    Update permit.sh

--- linuxtask1/permit.sh
+++ linuxtask1/permit.sh
@@ -2,36 +2,36 @@
 
 sudo groupadd warden
 sudo groupadd student
-sudo groupadd GarnetA
-sudo groupadd GarnetB
-sudo groupadd Opal
-sudo groupadd Agate
 
 setfacl -R -m u:had:rwx /home
+setfacl -R -m u:root:rwx /home
 
 while read line; do
   name=$(echo "$line" | cut -d " " -f 1)
   hostel=$(echo "$line" | cut -d " " -f 3)
   room=$(echo "$line" | cut -d " " -f 4)
-  sudo usermod -aG student $name
-  sudo usermod -aG $hostel $name
-  setfacl -R -m u:$hostel:rwx /home/$hostel
-  setfacl -m u:$name:--- /home/$hostel/$room
-  setfacl -m u:$name:rwx /home/$hostel/$room/$name
+  
+  sudo usermod -aG student $name 
+  
+  setfacl -m u:$name:rwx /home/$hostel/$room
+  setfacl -R -m u:$name:rwx /home/$hostel/$room/$name
+  
+  setfacl -m u:$name:rwx /home/$hostel/$room
+  setfacl -m d:u:$name:rwx /home/$hostel/$room
+
+  setfacl -m u:$hostel:rwx /home/$hostel/$room/$name
+  setfacl -m u:$hostel:rwx /home/$hostel/$room/$name/userdetails.txt
 done < studentDetails.txt
 
 for i in "GarnetA" "GarnetB" "Opal" "Agate"; do
   sudo usermod -aG warden $i
-  sudo usermod -aG $i $i
-  chmod 700 /home/$i
-  setfacl -m g:$i:--x /home/$i
-  setfacl -m g:$i:r-- /home/$i/feeDefaulters.txt
-  setfacl -m g:$i:r-- /home/$i/announcements.txt
+  chmod 777 /home/$i/stayout.txt
+  setfacl -m g:warden:rwx /home/$i
+  setfacl -R -m u:$i:rwx /home/$i
+  setfacl -m u:$i:rwx /home/$i/feeDefaulters.txt
+  setfacl -m u:$i:rwx /home/$i/announcements.txt
 done
 
-
-chmod 700 /home/had
-
+setfacl -m g:student:--x /home
 setfacl -m g:student:--x /home/had
 setfacl -m g:student:rw- /home/had/mess.txt
-
