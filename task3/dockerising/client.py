60afbf10ecdbac3350d2f5b362dc9f45454199eb
pirate <132884539+GenericFace@users.noreply.github.com>
2023-08-04 11:02:23 +0530

    Update client.py

--- task3/dockerising/client.py
+++ task3/dockerising/client.py
@@ -3,34 +3,39 @@ import threading
 import psycopg2
 import sys
 
-username = input("Enter username: ")
+try:
+    conn = psycopg2.connect(
+        host="localhost",
+        port="3307",
+        database="postgres",
+        user="postgres",
+        password="1234"
+    )
+    cur = conn.cursor()
 
-conn = psycopg2.connect(
-    host="db",
-    database="postgres",
-    user="postgres",
-    password="1234"
-)
-cur = conn.cursor()
+    username = input("Enter username: ")
+    password = input("Enter your password: ")
 
-cmd = f"SELECT username FROM auth;"
-cur.execute(cmd)
-results = cur.fetchone()
+    cmd = f"SELECT username, password FROM auth WHERE username = %s"
+    cur.execute(cmd, (username,))
+    user_data = cur.fetchone()
 
-for result in results:
-    if username == result:
-        password = input("Enter your password: ")
-        cm = f"SELECT password FROM auth WHERE username='{username}'"
-        cur.execute(cm)
-        pwd = cur.fetchone()
-        if password == pwd[0]:
-            print("User authenticated!!!")
+    if user_data:
+        db_username, db_password = user_data
+        if password == db_password:
+            print("User authenticated!")
         else:
             print("Wrong password")
-            sys.exit()
     else:
         print("User not found")
-        sys.exit()
+
+    cur.close()
+    conn.close()
+
+except psycopg2.Error as e:
+    print(f"Error connecting to PostgreSQL: {e}")
+    sys.exit()
+
         
 choice = input("Do you want to upload (type UP) or download (type DOWN): ")
 if choice == "UP":
